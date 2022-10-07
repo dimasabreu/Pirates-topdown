396d139ff7a08bb1d52991130c75efd83913c294
Dimas Abreu <dimasabreu@live.com>
2022-10-06 23:36:40 -0300

    Update PlayerShooting.cs

--- Pirates/Assets/Scripts/PlayerScript/PlayerShooting.cs
+++ Pirates/Assets/Scripts/PlayerScript/PlayerShooting.cs
@@ -4,6 +4,12 @@ public class PlayerShooting : MonoBehaviour
 {
     [SerializeField] private GameObject cannonShoot;
     [SerializeField] private Transform frontalShoot;
+    [SerializeField] private Transform leftShoot1;
+    [SerializeField] private Transform leftShoot2;
+    [SerializeField] private Transform leftShoot3;
+    [SerializeField] private Transform rightShoot1;
+    [SerializeField] private Transform rightShoot2;
+    [SerializeField] private Transform rightShoot3;
     [SerializeField] public float fireDelay = 1f;
     private float cooldownTimer = 0;
     
