6aff1a4a391777d01f67d7c2f0fcce3ee87943a9
Dimas Abreu <dimasabreu@live.com>
2022-10-07 16:11:40 -0300

    Update PlayerShooting.cs

--- Pirates/Assets/Scripts/PlayerScript/PlayerShooting.cs
+++ Pirates/Assets/Scripts/PlayerScript/PlayerShooting.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerShooting : MonoBehaviour
 {
     [SerializeField] private GameObject cannonShoot;
+    [SerializeField] private GameObject cannonShoot2;
     [SerializeField] private Transform frontalShoot;
     [SerializeField] private Transform leftShoot1;
     [SerializeField] private Transform leftShoot2;
@@ -19,6 +20,20 @@ public class PlayerShooting : MonoBehaviour
         if (Input.GetButtonDown("Fire1") && cooldownTimer <= 0)
         {
             Instantiate(cannonShoot, frontalShoot.position, transform.rotation);
+            cooldownTimer = fireDelay;
+        }
+        if (Input.GetKeyDown(KeyCode.Q) && cooldownTimer <= 0)
+        {
+            Instantiate(cannonShoot, leftShoot1.position, leftShoot1.rotation);
+            Instantiate(cannonShoot, leftShoot2.position, leftShoot2.rotation);
+            Instantiate(cannonShoot, leftShoot3.position, leftShoot3.rotation);
+            cooldownTimer = fireDelay;
+        }
+        if (Input.GetKeyDown(KeyCode.E) && cooldownTimer <= 0)
+        {
+            Instantiate(cannonShoot2, rightShoot1.position, rightShoot1.rotation);
+            Instantiate(cannonShoot2, rightShoot2.position, rightShoot2.rotation);
+            Instantiate(cannonShoot2, rightShoot3.position, rightShoot3.rotation);
             cooldownTimer = fireDelay;
         }
     }
