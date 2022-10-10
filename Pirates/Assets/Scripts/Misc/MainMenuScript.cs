0888ca4c5371bb14c0b3b2de59b373b2c8bd36de
Dimas Abreu <dimasabreu@live.com>
2022-10-10 08:01:46 -0300

    Update MainMenuScript.cs

--- Pirates/Assets/Scripts/Misc/MainMenuScript.cs
+++ Pirates/Assets/Scripts/Misc/MainMenuScript.cs
@@ -1,7 +1,19 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenuScript : MonoBehaviour
 {
+    private void Awake() 
+    {
+        // garantindo q só tem 1
+        int qnt = FindObjectsOfType<Canvas>().Length;
+        if (qnt > 1)
+        {
+            Destroy(gameObject);
+        }
+        // nao destruindo quando trocar de cena
+        DontDestroyOnLoad(gameObject);
+    }
     public void Exit()
     {
         Application.Quit();
